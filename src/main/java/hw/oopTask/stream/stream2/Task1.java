1a7fb654adb609d5ffb73a57b31e0f8d0490bbb1
LinaisLina <watashiwangelina@gmail.com>
2024-12-23 21:51:17 +0300

    Создала болванку для задания 1

---
+++ src/main/java/hw/oopTask/stream/stream2/Task1.java
@@ -0,0 +1,4 @@
+package hw.oopTask.stream.stream2;
+
+public class Task1 {
+}
---
+++ src/main/java/lesson/test/Main.java
@@ -0,0 +1,30 @@
+package lesson.test;
+import lombok.extern.jackson.Jacksonized;
+
+import java.net.*;
+import java.io.*;
+import java.util.Date;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        var i = "khjhj";
+        var t = 8;
+        enum pupu{
+            REWd,i;
+        }
+
+//        String url = "https://animego.org/";
+//        URL obj = new URL(url);
+//        HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+//        connection.setRequestMethod("GET");
+//        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+//        String inputLine;
+//        StringBuffer response = new StringBuffer();
+//
+//        while ((inputLine = in.readLine()) != null) {
+//            response.append(inputLine);
+//        }
+//        in.close();
+//
+//        System.out.println(response.toString());
+    }
+}
